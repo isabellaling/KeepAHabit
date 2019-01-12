d66a679f0d500d0c1db2d4800c1379de353d5a8b
Isabella Ling <isabellaling@icloud.com>
2019-01-12 18:35:59 +0800

    add dependencies for mybatis

---
+++ src/main/java/com/ttxm/keepahabit/KeepAHabitApplication.java
@@ -0,0 +1,14 @@
+package com.ttxm.keepahabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepAHabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepAHabitApplication.class, args);
+	}
+	
+}
+
