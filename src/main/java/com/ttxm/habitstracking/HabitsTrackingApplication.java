cfb3fd9fbce80dd11d4bc0734e639a8f4ef14336
Isabella Ling <isabellaling@icloud.com>
2019-01-12 21:09:36 +0800

    change project name to habits-tracking

--- src/main/java/com/ttxm/habitstracking/HabitsTrackingApplication.java
+++ src/main/java/com/ttxm/habitstracking/HabitsTrackingApplication.java
@@ -1,13 +1,13 @@
-package com.ttxm.keepahabit;
+package com.ttxm.habitstracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeepAHabitApplication {
+public class HabitsTrackingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KeepAHabitApplication.class, args);
+		SpringApplication.run(HabitsTrackingApplication.class, args);
 	}
 	
 }
--- src/main/java/com/ttxm/habitstracking/controller/HelloWorldController.java
+++ src/main/java/com/ttxm/habitstracking/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.ttxm.keepahabit.controller;
+package com.ttxm.habitstracking.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
